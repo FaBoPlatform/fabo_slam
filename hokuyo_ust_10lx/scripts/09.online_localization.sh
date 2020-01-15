a1c9a7fc89344ca30f5bc724e8a0a440a2396ad0
naisy <takanashi@gclue.jp>
2020-01-15 18:39:52 +0900

    update imu

--- hokuyo_ust_10lx/scripts/09.online_localization.sh
+++ hokuyo_ust_10lx/scripts/09.online_localization.sh
@@ -22,7 +22,7 @@ roslaunch hokuyo_ust_10lx lidar.launch ip_address:=$HOKUYO_UST_10LX &
 if "${USE_IMU}"; then
   roslaunch razor_imu_9dof imu.launch &
   sleep 20 # wait until imu launch
-  IMU="_IMU"
+  IMU="_imu"
 else
   sleep 5 # wait until lidar launch
   IMU=""
